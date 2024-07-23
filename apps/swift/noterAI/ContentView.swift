fb8068017eea45cc0bfc5a28ada836a23e68ac87
Himanshu-AT <aristech.official@gmail.com>
2024-07-23 12:00:41 +0530

    add swift ui

---
+++ apps/swift/noterAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  noterAI
+//
+//  Created by Himanshu on 7/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ apps/swift/noterAI/noterAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  noterAIApp.swift
+//  noterAI
+//
+//  Created by Himanshu on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct noterAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
